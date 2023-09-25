6d346448a5560126dba3793e4e8b4227df0931bb
ray <pickupfaith@gmail.com>
2023-09-25 18:47:07 +0800

    使用BS nav-tab，RJ的map()，tabIndex修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-
 import "./globals.css";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      {/* <body className={`${inter.className} bg-dark`}>{children}</body> */}
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,79 +1,69 @@
-"use client"
+
+// import "bootstrap/dist/js/bootstrap"
 import { useEffect } from 'react';
-import "bootstrap/dist/js/bootstrap"
+
+// components
+import Navbar from '@/components/Navbar'
 export default function Home() {
 	// useEffect(() => {
-	// 	import("bootstrap")
+	// 	import("bootstrap/dist/js/bootstrap")
 	// }, []);   //此方法也可以import BS.js
-
+	let fakeData = [
+		{
+			id: 'first',
+			name: 'Apple',
+			description: 'A round fruit with a smooth skin, often red, yellow, or green, with firm white flesh and pips in the middle.'
+		},
+		{
+			id: 'second',
+			name: 'Banana',
+			description: 'A long, curved fruit with a yellow skin and soft, sweet, white flesh inside.'
+		},
+		{
+			id: 'third',
+			name: 'Cherry',
+			description: 'A small, round, soft fruit with a stone in the middle, usually red or black.'
+		},
+		{
+			id: 'fourth',
+			name: 'Date',
+			description: 'A sweet brownish fruit from an Old World palm.'
+		}
+	];
 	return (
-		<main>
-			<button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-				Launch demo modal
-			</button>
+		<>
+			<Navbar />
 
+			<nav>
+				<div className="nav nav-tabs justify-content-center" id="nav-tab" role="tablist">
+					{/* <button className="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Home</button> */}
+					{
+						fakeData.map(
+							(data) => (
+								<>
+									<button className="nav-link" id={`nav-${data.id}-tab`} data-bs-toggle="tab" data-bs-target={`#nav-${data.id}`} type="button" role="tab" aria-controls="nav-profile" aria-selected="false">{data.name}</button>
+								</>
+							)
+						)
+					}
+				</div>
+			</nav>
+			<div className="tab-content" id="nav-tabContent">
+				{/* <div className="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab" tabIndex="0">...</div> */}
+				{
+					fakeData.map(
+						(data) => (
+							<>
+								<div className="tab-pane fade" id={`nav-${data.id}`} role="tabpanel" aria-labelledby={`nav-${data.id}-tab`} tabIndex="0">
+									{data.description}
+								</div>
+							</>
+						)
+					)
+				}
 
-			<div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-				<div className="modal-dialog">
-					<div className="modal-content">
-						<div className="modal-header">
-							<h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-							<button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-						</div>
-						<div className="modal-body">
-							...
-						</div>
-						<div className="modal-footer">
-							<button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-							<button type="button" className="btn btn-primary">Save changes</button>
-						</div>
-					</div>
-				</div>
 			</div>
-			<h2>It's a beautiful day.</h2>
-			<p className="d-inline-flex gap-1">
-				<a className="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-					Link with href
-				</a>
-				<button className="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-					Button with data-bs-target
-				</button>
-			</p>
-			<div className="collapse" id="collapseExample">
-				<div className="card card-body">
-					Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
-				</div>
-			</div>
-			<h2>It's a beautiful day.</h2>
-			<a className="btn btn-primary" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
-				Link with href
-			</a>
-			<button className="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
-				Button with data-bs-target
-			</button>
-
-			<div className="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
-				<div className="offcanvas-header">
-					<h5 className="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
-					<button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-				</div>
-				<div className="offcanvas-body">
-					<div>
-						Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
-					</div>
-					<div className="dropdown mt-3">
-						<button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
-							Dropdown button
-						</button>
-						<ul className="dropdown-menu">
-							<li><a className="dropdown-item" href="#">Action</a></li>
-							<li><a className="dropdown-item" href="#">Another action</a></li>
-							<li><a className="dropdown-item" href="#">Something else here</a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</main>
+		</>
 
 	)
 }

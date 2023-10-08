82a8f02e73f01e00cbfce331e9bc309ce2e2e5eb
Taka Wu <pickupfaith@gmail.com>
2023-10-09 02:06:45 +0800

    fix:tab切換，Swiper 初版設定 完成

--- app/page.tsx
+++ app/page.tsx
@@ -32,32 +32,7 @@ export default function Home() {
 	return (
 		<>
 			<Navbar />
-			<div className='d-flex'>
-				<nav>
-					<div className="nav nav-tabs d-flex flex-column justify-content-center" id="nav-tab" role="tablist">
-						{/* <button className="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Home</button> */}
-						{
-							fakeData.map(
-								(data) => (
-									<button key={data.id} className="nav-link" id={`nav-${data.id}-tab`} data-bs-toggle="tab" data-bs-target={`#nav-${data.id}`} type="button" role="tab" aria-controls="nav-profile" aria-selected="false">{data.name}</button>
-								)
-							)
-						}
-					</div>
-				</nav>
-				<div className="tab-content" id="nav-tabContent">
-					{/* <div className="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab" tabIndex="0">...</div> */}
-					{
-						fakeData.map(
-							(data) => (
-								<div key={data.id} className="tab-pane fade" id={`nav-${data.id}`} role="tabpanel" aria-labelledby={`nav-${data.id}-tab`} tabIndex="0">
-									{data.description}
-								</div>
-							)
-						)
-					}
-				</div>
-			</div>
+			
 			<Catalog />
 		</>
 	)
--- components/Catalog.tsx
+++ components/Catalog.tsx
@@ -1,33 +1,89 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useRef, useState } from 'react';
+import Link from 'next/link';
+import FoodSelect from './FoodSelect';
+import './Catalog.css';
 
-import Link from 'next/link'
-import FoodSelect from './FoodSelect'
+type SectionRefs = {
+  [key: string]: React.RefObject<HTMLDivElement>;
+};
 
 export default function Catalog() {
-    return (
-        <>
-            <div className="d-flex  vh-100">
-                <div className=" bg-primary-subtle">
-                    <ul>
-                        <Link href="#section1" className='active'><li>Food 1</li></Link>
-                        <Link href="#section2"><li>Food 2</li></Link>
-                        <Link href="#section3"><li>Food 3</li></Link>
-                    </ul>
-                </div>
-                <div className='w-50 h-100'>
-                    <div id="section1" className="section">Section 1 Content...
-                    <FoodSelect/>
-                    </div>
-                    <div id="section2" className="section">Section 2 Content...
-                    <FoodSelect/>
-                    </div>
-                    <div id="section3" className="section">Section 3 Content...
-                    <FoodSelect/>
-                    </div>
-                </div>
+    const [currentSection, setCurrentSection] = useState<string | null>('section1');
 
-            </div>
-           
-        </>
-    )
+  const sectionRefs: SectionRefs = {
+    section1: useRef<HTMLDivElement>(null),
+    section2: useRef<HTMLDivElement>(null),
+    section3: useRef<HTMLDivElement>(null),
+  };
+
+  const handleScroll = () => {
+    let current = '';
+
+    for (const sectionName in sectionRefs) {
+        sectionRefs[sectionName].current
+      if (sectionRefs[sectionName].current) {
+        const section = sectionRefs[sectionName].current!;
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (window.scrollY >= sectionTop - sectionHeight / 3 && window.scrollY < sectionTop + sectionHeight - sectionHeight / 3) {
+          current = sectionName;
+          break;
+        }
+      }
+    }
+
+    setCurrentSection(current);
+  };
+
+  useEffect(() => {
+    // 添加被動事件監聽器
+    window.addEventListener('scroll', handleScroll, { passive: true });
+  
+    // 清除時移除事件監聽器
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="sidbar-container vh-100">
+        <div className="sidebar bg-primary-subtle">
+          <ul>
+            <li>
+              <Link href="#section1"  className={currentSection === 'section1' ? 'active' : ''}>
+                Food 1
+              </Link>
+            </li>
+            <li>
+              <Link href="#section2" passHref className={currentSection === 'section2' ? 'active' : ''}>
+                Food 2
+              </Link>
+            </li>
+            <li>
+              <Link href="#section3" passHref className={currentSection === 'section3' ? 'active' : ''}>
+                Food 3
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="content w-50 h-100">
+          <div ref={sectionRefs.section1} id="section1" className="section h-100">
+            Section 1 Content...
+            <FoodSelect />
+          </div>
+          <div ref={sectionRefs.section2} id="section2" className="section h-100 mt-1">
+            Section 2 Content...
+            <FoodSelect />
+          </div>
+          <div ref={sectionRefs.section3} id="section3" className="section h-100 mt-1">
+            Section 3 Content...
+            <FoodSelect />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
+
